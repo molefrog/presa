d7c47acd35b161fc7932a7057637deaeffcdae4f
Alexey Taktarov <molefrog@gmail.com>
2017-12-01 22:49:15 +0300

    Define theme defaults.

---
+++ src/theme.js
@@ -0,0 +1,32 @@
+export default {
+  // Settings related to slide content appearance
+  slide: {
+    baseFontSize: 22
+  },
+
+  // Using web-safe font defaults: base serif font and monospace
+  baseFont:
+    '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, ' +
+    'Ubuntu, Cantarell, "Helvetica Neue", sans-serif',
+
+  monoFont:
+    '"SF Mono", "Monaco", "Inconsolata", "Fira Mono", "Droid Sans Mono", ' +
+    '"Source Code Pro", monospace',
+
+  /* Color palette */
+  // Slideshow background
+  backgroundColor: '#fafafa',
+
+  // Used as an accent color in
+  // active elements
+  primaryColor: '#3c59ff',
+
+  // Default text color
+  textColor: '#222222',
+
+  // Icon background
+  darkGrayColor: '#5a5a5a',
+
+  // Stardust gray
+  mutedTextColor: '#9E9E9E'
+}
