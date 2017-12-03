67939e6c9a1f932bc7ca905ced9348ecb54ab012
Alexey Taktarov <molefrog@gmail.com>
2017-12-03 19:44:19 +0300

    TOC: auto scroll to the current slide.

--- src/components/slideshow-mode/toc.js
+++ src/components/slideshow-mode/toc.js
@@ -1,7 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const autoScroll = true
+
 class Toc extends React.Component {
+  componentDidUpdate(prevProps) {
+    const { currentSlide } = this.props
+    if (prevProps.currentSlide !== currentSlide) {
+      this.scrollToItem(currentSlide)
+    }
+  }
+
+  componentDidMount() {
+    this.scrollToItem(this.props.currentSlide)
+  }
+
+  scrollToItem(index) {
+    const _toc = this._tocEl
+
+    if (!autoScroll || !_toc) {
+      return
+    }
+
+    const item = _toc.querySelector(`[data-index="${index}"]`)
+    if (item && item.scrollIntoView) {
+      item.scrollIntoView({ block: 'center' })
+    }
+  }
+
   render() {
     const { className, slides, currentSlide, switchSlide } = this.props
 
@@ -9,13 +35,14 @@ class Toc extends React.Component {
       <NavigationBody className={className}>
         <PresentationName>{this.props.presentationName}</PresentationName>
 
-        <TableOfContents>
+        <TableOfContents innerRef={el => (this._tocEl = el)}>
           {slides.map((slide, index) => (
             <SlideItem
               key={index}
               current={index === currentSlide}
               future={index > currentSlide}
               onClick={() => switchSlide(index)}
+              data-index={index}
             >
               {slide.name || `Slide #${index + 1}`}
             </SlideItem>
@@ -66,7 +93,7 @@ const SlideItem = styled.div`
     props.current &&
     `
     color: black;
-    padding-left: 6px;
+    padding-left: 8px;
   `} ${props =>
       props.future &&
       `
