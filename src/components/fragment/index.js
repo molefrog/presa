ab28c483b1d77a664bcf9e65fdbfb99a60978956
Alexey Taktarov <molefrog@gmail.com>
2018-04-05 00:17:37 +0300

    Be able to test fragments feature.

--- src/components/fragment/index.js
+++ src/components/fragment/index.js
@@ -1,22 +1,38 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 import { FragmentConsumer } from '../slide/context'
 import styled from 'styled-components'
 
+const OpacityBehaviour = styled.div`
+  transition: opacity 0.2s ease-in;
+  display: ${props => (props.inline ? 'inline-block' : 'block')};
+  opacity: ${props => (props.active ? 1 : 0)};
+`
+
 class Fragment extends React.Component {
+  static propTypes = {
+    behaviour: PropTypes.any
+  }
+
+  static defaultProps = {
+    behaviour: OpacityBehaviour
+  }
+
   render() {
-    const { children, index } = this.props
+    const { index, behaviour, ...restProps } = this.props
+
+    const Behaviour = behaviour
 
     return (
       <FragmentConsumer>
-        {fragmentIndex => <SlideName hidden={index > fragmentIndex}>{children}</SlideName>}
+        {fragmentIndex => {
+          const isActive = index <= fragmentIndex
+          return <Behaviour {...restProps} active={isActive} />
+        }}
       </FragmentConsumer>
     )
   }
 }
 
-const SlideName = styled.div`
-  transition: opacity .2s ease-in;
-  display: ${props => props.inline ? 'inline-block' : 'block'};
-  opacity: ${props => props.hidden ? 0 : 1};
-`
 export default Fragment
---
+++ tests/fragments.test.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import TestRenderer from 'react-test-renderer'
+import TestContainer from './support/test-container'
+
+import RegularFragment from '../src/components/fragment'
+import Slide from '../src/components/slide/slide-decl'
+
+// Create TestFragment: it does not render content when not active
+// Why? easier to test.
+const Hideable = props => (props.active ? <div tag={props.children} /> : null)
+const Fragment = props => <RegularFragment {...props} behaviour={Hideable} />
+
+describe('fragments feature', () => {
+  it('displays a currently active fragment and all previous', () => {
+    const root = TestRenderer.create(
+      <TestContainer>
+        <Slide fragmentIndex={1}>
+          <Fragment index={0}>visible</Fragment>
+          <Fragment index={1}>visible</Fragment>
+          <Fragment index={2}>not yet visible</Fragment>
+        </Slide>
+      </TestContainer>
+    ).root
+
+    expect(root.findAllByProps({ tag: 'visible' }).length).toEqual(2)
+    expect(root.findAllByProps({ tag: 'not yet visible' }).length).toEqual(0)
+  })
+})
